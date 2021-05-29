35430e03ce9fa598f90b9fc5fde49f1d2643b827
nisarg0 <nisarggogate212@gmail.com>
2021-05-29 12:49:17 +0530

    Added Oauth Api pnly signup

--- Backend/node-auth/routes/user.js
+++ Backend/node-auth/routes/user.js
@@ -12,34 +12,6 @@ const User = require("../model/User");
  * @description - User SignUp
  */
 
-// router.post("/signup", async (req, res) => {
-// 	// const { username, email, password } = req.body;
-// 	console.log(req.body);
-// 	// user = new User({
-// 	// 	username,
-// 	// 	email,
-// 	// 	password,
-// 	// });
-
-// 	// await user.save();
-
-// 	res.send("success");
-
-// 	// jwt.sign(
-// 	// 	payload,
-// 	// 	"randomString",
-// 	// 	{
-// 	// 		expiresIn: 10000,
-// 	// 	},
-// 	// 	(err, token) => {
-// 	// 		if (err) throw err;
-// 	// 		res.status(200).json({
-// 	// 			token,
-// 	// 		});
-// 	// 	}
-// 	// );
-// });
-
 router.post(
 	"/signup",
 	[
